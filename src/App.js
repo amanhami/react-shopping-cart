191a2d59dfc2add8819edad9e7dc996690900209
Aman <amanueltek19@gmail.com>
2020-08-29 03:16:22 -0400

    cart component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,39 @@ import React from "react";
 import data from "./data.json";
 import Products from "./components/Products";
 import Filter from "./components/Filter";
+import Cart from "./components/Cart";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       products: data.products,
+      cartItems: [],
       size: "",
       sort: "",
     };
   }
+  removeFromCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    
+    this.setState({cartItems: cartItems.filter(x => x._id !== product._id)})
+  }
+  addToCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart = false;
+    cartItems.forEach((item) => {
+      if (item._id === product._id) {
+        item.count++;
+        alreadyInCart = true;
+      }
+    });
+    if (!alreadyInCart) {
+      cartItems.push({ ...product, count: 1 });
+    }
+    this.setState({
+      cartItems: cartItems,
+    });
+  };
   sortProducts = (e) => {
     // imp
     const sort = e.target.value;
@@ -22,13 +45,13 @@ class App extends React.Component {
         .slice()
         .sort((a, b) =>
           sort === "lowest"
-            ? ((a.price > b.price)
+            ? a.price > b.price
               ? 1
-              : -1)
+              : -1
             : sort === "highest"
-            ? ((a.price < b.price)
+            ? a.price < b.price
               ? 1
-              : -1)
+              : -1
             : a._id < b._id
             ? 1
             : -1
@@ -70,9 +93,14 @@ class App extends React.Component {
                 filterProducts={this.filterProducts}
                 sortProducts={this.sortProducts}
               />
-              <Products products={this.state.products} />
+              <Products
+                products={this.state.products}
+                addToCart={this.addToCart}
+              />
             </div>
-            <div className="sidebar">Cart Items</div>
+            <div className="sidebar">
+              <Cart cartItems={this.state.cartItems} removeFromCart={this.removeFromCart} />
+            </div>
           </div>
         </main>
         <footer>All right is reserved.</footer>
--- src/components/Products.js
+++ src/components/Products.js
@@ -20,7 +20,7 @@ export default class Products extends Component {
                     {formatCurrency(product.price)}
 
                   </div>
-                  <button className="button primary">Add To Cart</button>
+                  <button onClick={()=>this.props.addToCart(product)} className="button primary">Add To Cart</button>
 
                 </div>
               </div>
